cd0a3d531ef9bf845d2f68d97d86597f9bd09e6e
Pomelo <liangrenhong2017@gmail.com>
2019-12-13 17:58:16 +0800

    update
    update

--- src/markdownEditor/MarkdownEditor.js
+++ src/markdownEditor/MarkdownEditor.js
@@ -166,6 +166,7 @@ export default class MarkdownEditor extends React.Component {
     let code = this.getUrlVars("code", "");
     fetch("https://github.com/login/oauth/access_token", {
       method: "POST",
+      mode: "cors",
       headers: {
         "Content-Type": "application/json"
       },
