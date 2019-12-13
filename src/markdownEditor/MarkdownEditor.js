6dd39725e3baaa9027601919ee099196ae1a2a63
Pomelo <liangrenhong2017@gmail.com>
2019-12-13 17:50:39 +0800

    update
    update

--- src/markdownEditor/MarkdownEditor.js
+++ src/markdownEditor/MarkdownEditor.js
@@ -183,12 +183,13 @@ export default class MarkdownEditor extends React.Component {
 
   getUrlVars() {
     var vars = {};
-    var parts = window.location.href.replace(
-      /[?&]+([^=&]+)=([^&]*)/gi,
-      function(m, key, value) {
-        vars[key] = value;
-      }
-    );
+    window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(
+      m,
+      key,
+      value
+    ) {
+      vars[key] = value;
+    });
     return vars;
   }
 
